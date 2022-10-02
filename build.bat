2bab7e8c7c58f0b7ec635bc3404efe57b90c014e
zh3 jeff <zheyu@outlook.com>
2022-10-03 02:30:52 +0800

    修改错误

--- build.bat
+++ build.bat
@@ -5,8 +5,8 @@ rd /s/q .\releases
 ) 
 md releases\SHU-HealthRoadSubmitiAssistant\Configuration
 md releases\SHU-HealthRoadSubmitiAssistant\Resources
-xcopy \Configuration releases\SHU-HealthRoadSubmitiAssistant\Configuration
-xcopy \Resources releases\SHU-HealthRoadSubmitiAssistant\Resources
+xcopy Configuration releases\SHU-HealthRoadSubmitiAssistant\Configuration
+xcopy Resources releases\SHU-HealthRoadSubmitiAssistant\Resources
 go build -o releases\SHU-HealthRoadSubmitiAssistant\SHUHR.exe  
 cd releases
 tar -cvf SHU-HealthRoadSubmitiAssistant-releases.zip SHU-HealthRoadSubmitiAssistant  
